46d539ef8620e371b7b813e7542e60ff4230aab1
Pat Kaehuaea <>
2023-08-20 16:53:21 -0700

    added helm reconcile to redo script

--- scripts/redo.sh
+++ scripts/redo.sh
@@ -4,3 +4,5 @@ flux reconcile -n flux-system source git home-kubernetes
 flux reconcile kustomization cluster-apps-cloudflared
 helm get values cloudflare -n networking
 kubectl get kustomizations.kustomize.toolkit.fluxcd.io -A
+flux reconcile helmrelease cloudflare -n networking
+k logs -f -l app.kubernetes.io/name=cloudflare-tunnel -n networking
