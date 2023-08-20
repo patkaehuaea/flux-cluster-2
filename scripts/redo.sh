d7f268a8eb9e2c9756af49cfec8a21edfbeff40f
Pat Kaehuaea <>
2023-08-20 14:06:15 -0700

    booo

--- scripts/redo.sh
+++ scripts/redo.sh
@@ -2,5 +2,5 @@
 
 flux reconcile -n flux-system source git home-kubernetes
 flux reconcile kustomization cluster-apps-cloudflared
-helm get values cloudflared -n networking
-k get kustomizations.kustomize.toolkit.fluxcd.io -A
+helm get values cloudflare -n networking
+kubectl get kustomizations.kustomize.toolkit.fluxcd.io -A
